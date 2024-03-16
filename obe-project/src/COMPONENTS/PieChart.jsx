c6a37cef7147e3f1e68857bf84145b0bba747f9e
abrarCSE29 <jucse29.398@gmail.com>
2024-03-17 02:59:48 +0600

    Create Piechart

--- obe-project/src/COMPONENTS/PieChart.jsx
+++ obe-project/src/COMPONENTS/PieChart.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
-import { Box } from '@mui/material'
+import { Box ,Typography } from '@mui/material'
+import { PieChart, pieArcLabelClasses } from '@mui/x-charts/PieChart';
 
-export default function PieChart(incourse) {
+const data = [
+  { value: 9, label: '80%-100%' },
+  { value: 26, label: '70%-79%' },
+  { value: 16, label: '60%-69%' },
+  { value: 3, label: '50%-59%' },
+  { value: 0, label: '40%-49%' },
+  { value: 0, label: '<40%' },
+];
+
+const size = {
+  width: 500,
+  height: 300,
+};
+
+export default function PieChart_({incourse}) {
   return (
     <Box>
-      Pie Chart
+      <Typography>{incourse}</Typography>
+      <PieChart
+
+      series={[
+        {
+          data,
+          highlightScope: { faded: 'global', highlighted: 'item' },
+          faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
+          arcLabel: (item) => `${item.label} `,
+          arcLabelMinAngle: 45,
+          
+        },
+      ]}
+      sx={{
+        [`& .${pieArcLabelClasses.root}`]: {
+          fill: 'white',
+          fontWeight: 'bold',
+        },
+      }}
+      {...size}
+    />
+
     </Box>
   )
 }
