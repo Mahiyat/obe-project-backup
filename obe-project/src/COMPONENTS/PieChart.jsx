659bf1c02f3f5da2eeb9f7dd517ce098873fd8eb
Mahiyat <mahiyat.tanzim@gmail.com>
2024-05-16 11:24:11 +0600

    Fix pie chart design
    Add useEffect hook

--- obe-project/src/COMPONENTS/PieChart.jsx
+++ obe-project/src/COMPONENTS/PieChart.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { Box ,Typography } from '@mui/material'
-import { PieChart, pieArcLabelClasses } from '@mui/x-charts/PieChart';
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+import { PieChart } from '@mui/x-charts/PieChart';
 
 const data = [
   { value: 9, label: '80%-100%' },
@@ -16,36 +16,30 @@ const size = {
   height: 300,
 };
 
-export default function PieChart_({incourse}) {
+export default function PieChart_({ incourse }) {
   return (
-    <Box
-     paddingTop={10}
-    >
-      
-      <PieChart
-
-      series={[
-        {
-          data,
-          highlightScope: { faded: 'global', highlighted: 'item' },
-          faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
-          arcLabel: (item) => `${item.label} `,
-          arcLabelMinAngle: 45,
-          
-        },
-      ]}
-      sx={{
-        [`& .${pieArcLabelClasses.root}`]: {
-          fill: 'white',
-          fontWeight: 'bold',
-        },
-      }}
-      {...size}
-    />
-    <Typography
-      paddingTop={4}
-      variant='h5'
-    >{incourse}</Typography>
+    <Box>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          paddingY: '12px',
+        }}
+      >
+        <PieChart
+          series={[
+            {
+              data,
+              highlightScope: { faded: 'global', highlighted: 'item' },
+              faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
+              arcLabelMinAngle: 45,
+            },
+          ]}
+          {...size}
+        />
+      </Box>
+      <Typography>{incourse}</Typography>
     </Box>
-  )
+  );
 }
