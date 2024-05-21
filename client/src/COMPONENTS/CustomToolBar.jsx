1ea9ec4fa233b05edd81f3a1d77c45bd885a8642
Mahiyat <mahiyat.tanzim@gmail.com>
2024-05-21 12:01:41 +0600

    Adjust print view of custom toolbar in marksheets

--- client/src/COMPONENTS/CustomToolBar.jsx
+++ client/src/COMPONENTS/CustomToolBar.jsx
@@ -1,11 +1,43 @@
-import { GridToolbarContainer, GridToolbarExport, GridToolbarFilterButton } from '@mui/x-data-grid'
-import React from 'react'
+import {
+  GridToolbarContainer,
+  GridToolbarExport,
+  GridToolbarFilterButton,
+} from '@mui/x-data-grid';
+import React from 'react';
 
 export default function CustomToolBar() {
   return (
     <GridToolbarContainer>
-      <GridToolbarExport />
+      <GridToolbarExport
+        printOptions={{
+          hideFooter: true,
+          hideToolbar: true,
+          pageStyle: `
+            @media print {
+              body {
+                -webkit-print-color-adjust: exact;
+              }
+              .MuiDataGrid-root .MuiDataGrid-main {
+                width: 100%;
+                max-width: 100%;
+                overflow: visible;
+                transform: scale(0.8);
+                transform-origin: top left;
+              }
+              .MuiDataGrid-root .MuiDataGrid-windowContainer {
+                overflow: visible !important;
+              }
+            }
+            @page {
+              size: landscape;
+              margin: 0mm;  /* this affects the margin in the printer settings */
+              overflow: visible;
+              scale: custom;
+            }
+          `,
+        }}
+      />
       <GridToolbarFilterButton />
     </GridToolbarContainer>
-  )
+  );
 }
