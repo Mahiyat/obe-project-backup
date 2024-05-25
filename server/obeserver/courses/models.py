2a175066ed99a0517fd3ce6e9901e74aa4d7c4d3
Mahiyat <mahiyat.tanzim@gmail.com>
2024-05-25 19:37:10 +0600

    Rename active_status field to completed_status

--- server/obeserver/courses/models.py
+++ server/obeserver/courses/models.py
@@ -5,6 +5,6 @@ class Course(models.Model):
   course_id=models.CharField(max_length=10, blank=True, null=True)
   course_name=models.CharField(max_length=40, blank=True, null=True)
   exam_title=models.CharField(max_length=60, blank=True, null=True)
-  active_status=models.BooleanField(default=False)
+  completed_status=models.BooleanField(default=False)
   def __str__(self) -> str:
     return self.course_id + ": "+self.exam_title
