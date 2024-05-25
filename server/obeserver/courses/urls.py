d40d00e9a4f0cdf00b20d95b85ce6d115a827c1f
Mahiyat <mahiyat.tanzim@gmail.com>
2024-05-25 21:50:38 +0600

    Add APIs for fetching completed courses and pending courses

--- server/obeserver/courses/urls.py
+++ server/obeserver/courses/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     re_path('$',views.show,name='show'),
+    re_path('completed$',views.get_completed_courses,name='get_completed_courses'),
+    re_path('pending$',views.get_pending_courses,name='get_pending_courses'),
     re_path('(\d{1,2})$', views.detail, name='detail'),
     re_path('status/(\d{1,2})$', views.change_status, name='change_status'),
 ]
